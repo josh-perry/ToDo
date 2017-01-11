2ecc0c9d8fedc08ad23f64386e158deeddd88514
Josh Perry <joshperry@outlook.com>
2017-01-11 15:11:17 +0000

    :sparkles: :lipstick: :heavy_plus_sign: Added handlebars-v4.0.5.js Created a template for to do item Some styling

---
+++ src/ToDo/wwwroot/js/todo.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    console.log("Ready");
+
+    var source = $("#todo-item-template").html();
+    var template = Handlebars.compile(source);
+
+    var items = $("#todo-items");
+
+    for (var i = 0; i < 4; i++) {
+        var context = {
+            description: "Lorem ipsum " + i
+        }
+
+        var html = template(context);
+        items.append(html);
+    }
+});
