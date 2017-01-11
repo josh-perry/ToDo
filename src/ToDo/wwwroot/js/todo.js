a56730decc33f6bc7725fba486927105e8df9fc5
Josh Perry <joshperry@outlook.com>
2017-01-11 15:33:04 +0000

    :sparkles: Added item IDs Added strike-through toggle when done button is clicked Added delete button with fading

--- src/ToDo/wwwroot/js/todo.js
+++ src/ToDo/wwwroot/js/todo.js
@@ -1,5 +1,19 @@
 ﻿$(document).ready(function () {
-    console.log("Ready");
+    // Source: https://jsfiddle.net/briguy37/2MVFd/
+    function generateUuid() {
+        var d = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random()*16)%16 | 0;
+            d = Math.floor(d/16);
+            return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+        });
+        return uuid;
+    };
+
+    function getItemByItemId(id) {
+        var item = $(".todo-item[item-id='" + id + "']");
+        return item;
+    }
 
     var source = $("#todo-item-template").html();
     var template = Handlebars.compile(source);
@@ -8,10 +22,37 @@
 
     for (var i = 0; i < 4; i++) {
         var context = {
-            description: "Lorem ipsum " + i
+            "description": "Lorem ipsum " + i,
+            "item-id": generateUuid()
         }
 
         var html = template(context);
         items.append(html);
     }
+
+    // Click event listener for all item done buttons
+    $(document).on("click", ".todo-item-done", function () {
+        // Get item to strike through by it's item-id
+        var id = $(this).attr("item-id");
+        var item = getItemByItemId(id);
+
+        // Get the description from within the found item
+        var description = item.find(".todo-item-description");
+
+        // Toggle strike-through
+        description.toggleClass("strikeout");
+    });
+
+    // Click event listener for all item delete buttons
+    $(document).on("click", ".todo-item-delete", function () {
+        // Get item to remove by it's item-id
+        var id = $(this).attr("item-id");
+        var item = getItemByItemId(id);
+
+        // Fade out
+        item.fadeOut("fast", function () {
+            // Remove once fade out is complete
+            item.remove();
+        });
+    });
 });
